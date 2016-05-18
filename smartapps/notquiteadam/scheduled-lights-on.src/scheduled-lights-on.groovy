8b6119f430b9aedca8e7781629a0945e5b06a485
Adam Aiello <adam.aiello@gmail.com>
2016-05-18 09:34:37 -0400

    SmartApp to schedule lights on at a specific time daily, and off with sunrise.

---
+++ smartapps/notquiteadam/scheduled-lights-on.src/scheduled-lights-on.groovy
@@ -0,0 +1,52 @@
+/**
+ *  Scheduled Lights
+ *
+ *  Copyright 2016 Adam Aiello
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Scheduled Lights On",
+    namespace: "NotQuiteAdam",
+    author: "Adam Aiello",
+    description: "Simple repeating schedule for lights at a specific time. ",
+    category: "Convenience",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+	section ("Daily time..."){
+    	input "theTime", "time", title: "Time to execute every day"
+    }
+    section ("Turn on these lights"){
+    	input "theSwitches", "capability.switch", multiple:true
+    }
+}
+
+def installed() {
+	initialize()
+}
+
+def updated() {
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	schedule(theTime, handler)
+}
+
+// called every day at the time specified by the user
+def handler() {
+    theSwitches.on()
+}
